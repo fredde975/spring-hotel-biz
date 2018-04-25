5c041fe6322841e010e37132f0b1d393d70f9459
Fredrik Thernelius <fredrik.thernelius@gmail.com>
2018-04-25 10:31:27 +0200

    first commit

---
+++ src/main/java/com/example/hotelbiz/HotelBizApplication.java
@@ -0,0 +1,12 @@
+package com.example.hotelbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBizApplication.class, args);
+    }
+}
